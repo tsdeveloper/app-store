7d04076ccbd7138dd7bc93a729a191ba8fdc2461
thiago <ts.analista@gmail.com>
2020-04-21 22:12:13 -0300

    fix: bug seed

---
+++ API/Seed/AcessoIngressoInitializer.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Bogus;
+
+namespace API.Seed
+{
+    public class AcessoIngressoInitializer
+    {
+        private readonly AcessoIngressoContext _context;
+        public AcessoIngressoInitializer(AcessoIngressoContext context)
+        {
+            _context = context;
+          
+        }
+
+        private void FactoryProducts()
+        {
+            if (!_context.Products.Any()){
+                var category = new Faker<Product>()
+                    .RuleFor(p => p.ProductId, p => p.Random.Guid())
+                    .RuleFor(p => p.Name, p => p.Commerce.Department())
+                    .Generate(4);
+                _context.Products.AddRange(category);
+                _context.SaveChanges();
+            }
+    }
+
+        internal void SeedDemo()
+        {
+            FactoryProducts();
+        }
+    }
+}
