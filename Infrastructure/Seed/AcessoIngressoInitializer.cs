c8c3792dd2f2c071555c684a25110c6512271591
thiago <ts.analista@gmail.com>
2020-04-23 00:18:38 -0300

    fix: update property product

--- Infrastructure/Seed/AcessoIngressoInitializer.cs
+++ Infrastructure/Seed/AcessoIngressoInitializer.cs
@@ -9,6 +9,7 @@ namespace Infrastructure.Seed
     public class AcessoIngressoInitializer
     {
         private readonly AcessoIngressoContext _context;
+
         public AcessoIngressoInitializer(AcessoIngressoContext context)
         {
             _context = context;
@@ -17,11 +18,12 @@ namespace Infrastructure.Seed
 
         private void FactoryProducts()
         {
+            
             if (!_context.Products.Any()){
                 var category = new Faker<Product>()
-                    .RuleFor(p => p.ProductId, p => p.Random.Guid())
+                    .RuleFor(p => p.Id, p => p.Random.Guid())
                     .RuleFor(p => p.Name, p => p.Commerce.Department())
-                    .Generate(4);
+                    .Generate(80000);
                 _context.Products.AddRange(category);
                 _context.SaveChanges();
             }
