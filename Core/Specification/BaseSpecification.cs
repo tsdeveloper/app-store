1dec402952f4a8082665612f2e5244f16e3abf67
thiago <ts.analista@gmail.com>
2020-04-27 20:51:09 -0300

    fix: add class specification for criteria and includes

---
+++ Core/Specification/BaseSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specification
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T, bool>> crieria)
+        {
+            Crieria = crieria;
+            
+        }
+
+        public Expression<Func<T, bool>> Crieria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = 
+            new List<Expression<Func<T, object>>>();
+
+        protected void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
---
+++ Core/Specification/ISpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specification
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Crieria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+        
+    }
+}
