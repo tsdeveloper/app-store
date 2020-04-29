d4218b7cd8ad8f66a384579e27791246b671ea0c
thiago <ts.analista@gmail.com>
2020-04-29 00:14:23 -0300

    fix: fix sevices collection

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -32,35 +33,14 @@ namespace API {
         public void ConfigureServices (IServiceCollection services) {
 
             
-            services.AddScoped<IProductRepository, ProductRepository> ();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers ();
             var connectionString = _config.GetConnectionString ("MSSQLConn");
             
             services.AddDbContext<AcessoIngressoContext> (o => o.UseSqlServer (connectionString, 
                                                 x => x.MigrationsAssembly("Migrations")));
-            services.Configure<ApiBehaviorOptions>(o =>
-            {
-                o.InvalidModelStateResponseFactory = actionConext =>
-                {
-                    var errors = actionConext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-                    var errorResponse = new ApliValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Acesso Ingresso API", Version = "v1"});
-            });
+            services.AddApplicationServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
