4dacccb63c044ed125aea0ceca198c5d0d1949c4
Build Code <you@example.com>
2020-04-30 00:11:49 -0300

    fix: add paginate

--- Core/Specification/ISpecification.cs
+++ Core/Specification/ISpecification.cs
@@ -10,6 +10,9 @@ namespace Core.Specification
         List<Expression<Func<T, object>>> Includes { get; }
         Expression<Func<T, object>> OrderBy { get; }
         Expression<Func<T, object>> OrderbyByDescending { get; }
+         int Take { get; }
+         int Skip { get; }
+         bool IsPagingEnabled { get; }
         
     }
 }
