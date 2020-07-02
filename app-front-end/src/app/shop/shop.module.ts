a5db3298e29246dde73eef9bedcfa376254214da
thiago <ts.analista@gmail.com>
2020-07-02 18:50:59 -0300

    fix: add component product-details. add router app

--- app-front-end/src/app/shop/shop.module.ts
+++ app-front-end/src/app/shop/shop.module.ts
@@ -4,15 +4,16 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from './../shared/shared.module';
 import { ShopComponent } from './shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [
     CommonModule,
     ShopRoutingModule,
     SharedModule
   ],
-  exports:[ShopComponent, ProductItemComponent]
+  exports:[ShopComponent, ProductItemComponent, ProductDetailsComponent]
 })
 export class ShopModule { }
