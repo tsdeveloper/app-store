a4a3a54e33334f1b1ff97dde44a1cca636603629
thiago <ts.analista@gmail.com>
2020-07-08 12:49:47 -0300

    fix: add buggy fake controller

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using API.Errors;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly AppStoreContext _context;
+
+        public BuggyController(AppStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFoundRequest()
+        {
+            var thing = _context.DbSet<Product>().Find(42);
+            if (thing != null)
+                return NotFound(new ApiResponse(404));
+
+            return Ok();
+        }
+
+        [HttpGet("servererror")]
+        public IActionResult GetServerError()
+        {
+            var thing = _context.DbSet<Product>().Find(42);
+
+            var thingToReturn = thing.ToString();
+            
+            return Ok();
+        }
+        
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(404));
+        }
+        
+        [HttpGet("badrequest/{id}")]
+        public IActionResult GetNotFoundRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
