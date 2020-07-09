7ba73fbfaebfc8aabe142c59eb6e79491efc3f67
thiago <ts.analista@gmail.com>
2020-07-09 11:19:32 -0300

    fix: add each erros validation and server error extras details

--- app-front-end/src/app/core/interceptors/error.interceptor.ts
+++ app-front-end/src/app/core/interceptors/error.interceptor.ts
@@ -1,6 +1,6 @@
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {Router} from '@angular/router';
+import {NavigationExtras, Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {catchError} from 'rxjs/operators';
 import {ToastrService} from 'ngx-toastr';
@@ -35,7 +35,8 @@ export class ErrorInterceptor implements HttpInterceptor {
                         this.router.navigateByUrl('/not-found');
                     }
                     if (error.status === 500) {
-                        this.router.navigateByUrl('/server-error');
+                        const navigationExtras: NavigationExtras = {state: {error: error.error}};
+                        this.router.navigateByUrl('/server-error', navigationExtras);
                     }
                 }
                 return throwError(error);
