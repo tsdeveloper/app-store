97be0f53eefaef29427bae1a732fda11e568cbd1
thiago <ts.analista@gmail.com>
2020-07-09 10:07:22 -0300

    fix: add toastr error

--- app-front-end/src/app/app.module.ts
+++ app-front-end/src/app/app.module.ts
@@ -1,31 +1,38 @@
-import { ShopModule } from './shop/shop.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ShopModule} from './shop/shop.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-import {HomeModule} from "./home/home.module";
+import {CoreModule} from './core/core.module';
+import {HomeModule} from './home/home.module';
 import {ErrorInterceptor} from './core/interceptors/error.interceptor';
+import {ToastrModule} from 'ngx-toastr';
 
 @NgModule({
-  declarations: [
-    AppComponent
-      ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    CoreModule,
-    HomeModule
-  ],
-  providers: [
-      {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
-      ]
-  ,
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        CoreModule,
+        HomeModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-right',
+            preventDuplicates: true
+        })
+
+    ],
+    providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    ]
+    ,
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- app-front-end/src/app/core/interceptors/error.interceptor.ts
+++ app-front-end/src/app/core/interceptors/error.interceptor.ts
@@ -1,17 +1,30 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {catchError} from 'rxjs/operators';
+import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(private router: Router) {}
+    constructor(private router: Router, private toastr: ToastrService) {}
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(
             catchError(error => {
                 if (error){
+
+                    if (error.status === 0 && !error.ok){
+                        this.toastr.error('Not found server');
+                    }
+
+                    if (error.status === 400){
+                        this.toastr.error(error.error.message, error.error.statusCode);
+                    }
+                    if (error.status === 401){
+                        this.toastr.error(error.error.message, error.error.statusCode);
+                    }
+
                     if (error.status === 404){
                         this.router.navigateByUrl('/not-found');
                     }
