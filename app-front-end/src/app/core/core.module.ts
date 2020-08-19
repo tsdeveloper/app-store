d1836f78885b7e7f5b27776140ec6fa931a30d02
thiago <thiago.almeida@ndd.com.br>
2020-08-18 23:10:07 -0300

    fix: add Breadcumps sections

--- app-front-end/src/app/core/core.module.ts
+++ app-front-end/src/app/core/core.module.ts
@@ -5,15 +5,17 @@ import {RouterModule} from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import {SectionHeaderComponent} from './section-header/section-header.component';
+
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
+  declarations: [NavbarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [NavbarComponent]
+  exports: [NavbarComponent, SectionHeaderComponent, ServerErrorComponent]
 })
 export class CoreModule { }
